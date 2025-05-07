f2e0576c841e55f46388e3fb940fba36d37f5dee
Alexander Lozano <alexanderlozano950@gmail.com>
2025-05-06 22:43:50 -0500

    structure app restaurant  microservices

---
+++ dicovery/src/main/java/com/dicovery/DicoveryApplication.java
@@ -0,0 +1,13 @@
+package com.dicovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryApplication.class, args);
+	}
+
+}
